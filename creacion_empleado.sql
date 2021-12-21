4fc5204e31490a2e543c27268c922f6541aba086
ferluque <fl1562001@gmail.com>
2021-12-21 15:28:21 +0100

    update

---
+++ creacion_empleado.sql
@@ -0,0 +1,11 @@
+create table empleado (
+    dni varchar2(9) constraint empleado_dni_CP primary key,
+    nombre varchar2(20) not null,
+    apellido varchar2(50) not null,
+    telefono varchar2(20) not null,
+    correo varchar2(30) not null,
+    domicilio varchar2(30) not null,
+    rol varchar2(30) not null,
+    salario float not null check (salario>0) 
+);
+
---
+++ vehiculos/creacion_vehiculo.sql
@@ -0,0 +1,3 @@
+create table vehiculo (
+    num_bastidor char()
+)
